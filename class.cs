716d655f2c890c9ad819aa56be10dc6192bbc12e
Liranitz <liranitz@post.bgu.ac.il>
2022-03-27 15:50:16 +0300

    create class.cs

---
+++ class.cs
@@ -0,0 +1,12 @@
+Public int compare(Actor other)
+{
+	Int ourNum = this.GetnumFilms();
+	Int otherNum = other.getnumFilms();
+	If(ourNum > otherNum)
+		Return 1;
+	If(OurNum < otherNum)
+		Return 2;
+	Else{
+		Return 3;
+	}
+}
